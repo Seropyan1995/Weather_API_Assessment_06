a37cc860de05d77d6b7034c706d1afa9348c38ab
Gevork Seropyan <seropyangevork@rocketmail.com>
2023-05-23 16:42:45 -0400

    updates

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-let 
+// HTML references for Global Variables
+let citySearch = $("#city-search");
+let submitBtn = $("#submit-button");
+// api key
+let key = "f70fcd8738d0bfa67a0c033bc83aab2d";
+
+let date = dayjs().format('dddd, MMMM D YYYY');
+let dateTime = dayjs().format('YYYY-MM-DD HH:MM:SS');
