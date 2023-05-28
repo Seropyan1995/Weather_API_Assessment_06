a1f7848612f9ae06e728ec0259ba60dd866fffe4
Gevork Seropyan <seropyangevork@rocketmail.com>
2023-05-28 17:34:43 -0400

    added seach form JS

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-// HTML references for Global Variables
 let citySearch = $("#city-search");
 let submitBtn = $("#submit-button");
 // api key
@@ -6,3 +5,26 @@ let key = "f70fcd8738d0bfa67a0c033bc83aab2d";
 
 let date = dayjs().format('dddd, MMMM D YYYY');
 let dateTime = dayjs().format('YYYY-MM-DD HH:MM:SS');
+
+let apiUrl = `api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`
+$(function(){
+    citySearchForm();
+})
+
+// holds an event listener that listens for the city search form submit
+function citySearchForm() {
+
+    citySearch.submit(function(event){
+        event.preventDefault();
+        var city = $('#new-city').val();
+        console.log('button clicked');
+        if(!city){
+            console.log('its blank')
+            return;
+        }
+    })
+    var city = $('#new-city').val();
+    
+};
+
+document
