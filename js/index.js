0e57d242c7916169398806d5e9b0e15aa2dc390e
Gevork Seropyan <seropyangevork@rocketmail.com>
2023-06-01 11:11:04 -0400

    working on city error

--- js/index.js
+++ js/index.js
@@ -1,42 +1,36 @@
-let citySearch = $("#city-search");
-let submitBtn = $("#submit-button");
-let key = "f70fcd8738d0bfa67a0c033bc83aab2d";
-let date = dayjs().format('dddd, MMMM D YYYY');
-let dateTime = dayjs().format('YYYY-MM-DD HH:MM:SS');
+var citySearch = $("#city-search");
+var submitBtn = $("#submit-button");
+var key = "f70fcd8738d0bfa67a0c033bc83aab2d";
+var date = dayjs().format('dddd, MMMM D YYYY');
+var dateTime = dayjs().format('YYYY-MM-DD HH:MM:SS');
 var cities = [];
-let apiUrl = `api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`
+
 
 $(function(){
+    defaultLoad();
+});
 
-})
+function cityButton() {
+    var cityBtn = $('.city');
 
-function hoistFunctions(){
-
-};
-
-function cityClick() {
-    var cityButton = $('.city');
-
-    cityButton.on("click", function (event) {
+    cityBtn.on("click", function (event) {
         event.preventDefault()
         city = $(event.target).text()
+        getCoordinates(city)
 });
 }
 
 function cityForm() {
-
     citySearch.submit(function(event){
         event.preventDefault();
-        var city = $('#new-city').val();
-        console.log('button clicked');
+        var city = $('#new-search').val();
         if(!city){
-            console.log('its blank')
             return;
         }
     })
-
+    getCoordinates(city);
 };
-// testing fetch lat lon 
+
 function fetchForecast(lat, lon) {
     fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat +  '&lon=' + lon + '&units=imperial&appid=' + key)
         .then(function(response) {
@@ -48,37 +42,57 @@ function fetchForecast(lat, lon) {
 
 }
 
+function getCoordinates(city) {
+
+    fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=' + key)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data){
+            var lat= data[0].lat
+            var lon= data[0].lon
+            addCity(city);
+            getCurrentWeather(lat, lon);
+            fetchForecast(lat, lon);
+        });
+};
+
+function getCurrentWeather(lat, lon) {
+    console.log("get current city weather with coordinates:" + lat + "/" + lon)
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat +  '&lon=' + lon + '&units=imperial&appid=' + key)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            displayCurrentWeather(data);
+        })
+};
+
 function displayCurrentWeather(data) {
-    console.log("display current weather works")
+    var todayCard = $("#todayCard")
+    $(todayCard).empty();
 
-    var currentCardBody = $("#current-card-body")
-    $(currentCardBody).empty();
-
-    $('#city-name-today').text(data.name)
-    $('#card-today-date').text(date);
+    $('#nameToday').text(data.name)
+    $('#todayDate').text(date);
 
     $('.icons').attr('src', 'https://openweathermap.org/img/w/' + data.weather[0].icon + '.png')
 
     var tempEl = $('<p>').text('Temperature: ' + data.main.temp + '°F');
-    currentCardBody.append(tempEl);
+    todayCard.append(tempEl);
 
     var windEl = $('<p>').text('Wind: ' + data.wind.speed + ' MPH');
-    currentCardBody.append(windEl);
+    todayCard.append(windEl);
 
     var humEl = $('<p>').text('Humidity: ' + data.main.humidity + '%')
-    currentCardBody.append(humEl);
+    todayCard.append(humEl);
 
 }
 
 function cityStorage() {
-    console.log("save cities local storage works")
-
     localStorage.setItem('cities', JSON.stringify(cities));
 }
 
 function updateList() {
-    console.log("update city list on page works");
-
     cities = JSON.parse(localStorage.getItem('cities'));
 
     var $cityList = $('#city-list');
@@ -91,6 +105,7 @@ function updateList() {
         citiesHtml += '</row>';
     }
     $cityList.html(citiesHtml);
+    cityButton();
 }
 
 function displayForecast(data) {
@@ -112,5 +127,78 @@ function displayForecast(data) {
 				forecast.push(testObj);
 			}
 		})
+        // creating cards on the screen
+        for (let i = 0; i < 5; i++) {
 
+			var divElCard = $('<div>');
+			divElCard.attr('class', 'card  bg-light mb-3 cardOne');
+			divElCard.attr('style', 'max-width: 215px;');
+			weekForecastEl.append(divElCard);
+
+			var divElHeader = $('<div>');
+			divElHeader.attr('class', 'card-header')
+			var dayFormat = dayjs(`${forecast[i].date}`).format('MM-DD-YYYY');
+			divElHeader.text(dayFormat);
+			divElCard.append(divElHeader)
+
+			var divElBody = $('<div>');
+			divElBody.attr('class', 'card-body');
+			divElCard.append(divElBody);
+
+			var divElIcon = $('<img>');
+			divElIcon.attr('class', 'icons');
+			divElIcon.attr('src', `https://openweathermap.org/img/wn/${forecast[i].icon}@2x.png`);
+			divElBody.append(divElIcon);
+
+
+			var pElTemp = $('<p>').text('Temperature: ' + forecast[i].temp + ' °F');
+			divElBody.append(pElTemp);
+
+
+			var pElHumid = $('<p>').text('Humidity: ' + forecast[i].humidity + ' %');
+			divElBody.append(pElHumid);
+
+            var pwindEl = $('<p>').text('Wind: ' +  forecast[i].wind + ' MPH');
+            divElBody.append(pwindEl);
+}};
+
+function addCity (city) {
+    var cityIndex = cities.indexOf(city);
+    if (cityIndex !== -1) {
+        cities.splice(cityIndex, 1);
+    }
+    cities.unshift(city)
+    cityStorage();
+    updateList();
+
+    $('#new-search').val("");
+}
+
+function defaultLoad(city) {
+    var citiesStored = JSON.parse(localStorage.getItem('cities'));
+
+    if (citiesStored === null) {
+        var city= "New York";
+
+            var cities = {
+                city: city
+            }
+            getCoordinates(city);
+            cityForm();
+
+    } else if (citiesStored !== null) {
+		var cities = citiesStored
+
+        for (var i=0; i < cities.length; i++) {
+
+    if (!cities) {
+        cities = [];
+        return;
+    };
+
+    getCoordinates(cities[0]);
+    updateList(cities[i]);
+    cityForm();
+    }
+    }
 };
